6a11bc9f960fc8ba26fa8f82d18179e1ba3d3855
pc-PC\pc <wangchenghong@persagy.com>
2019-04-25 14:26:29 +0800

    first commit

--- src/main/java/com/wch/constant/RabbitConstant.java
+++ src/main/java/com/wch/constant/RabbitConstant.java
@@ -14,5 +14,4 @@ public class RabbitConstant {
     public static final String QUEUE_BAIDU = "baidu";
     public static final String QUEUE_SINA = "sina";
     
-    public static final String EXCHANGE_NAME="wangchenghong";
 }
